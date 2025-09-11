229a80d0148e39c99abb7afb4fd544d5fa49ae5e
Philip Nielsen <philipnordnielsen@gmail.com>
2025-09-11 11:20:13 +0200

    Added inspector extensions and image capability for animal data

--- Assets/Core/Scripts/ScriptableObjects/AnimalData.cs
+++ Assets/Core/Scripts/ScriptableObjects/AnimalData.cs
@@ -1,7 +1,11 @@
+using Nenn.InspectorEnhancements.Runtime.Attributes.Conditional;
+using Nenn.InspectorEnhancements;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using Unity.VisualScripting;
 using UnityEngine;
+using Nenn.InspectorEnhancements.Runtime.Attributes;
 
 [CreateAssetMenu(fileName = "animalData", menuName = "Animals/Animal Data")]
 public class AnimalData : ScriptableObject
@@ -19,16 +23,21 @@ public class AnimalData : ScriptableObject
 }
 
 [Serializable]
-public struct QuizQuestion
+public class QuizQuestion
 {
     public string Question;
     public QuizAnswer[] Answers;
 }
 [Serializable]
-public struct QuizAnswer
+public class QuizAnswer
 {
     public string Answer;
+
+    public bool UsesImage;
+    public Texture2D Image;
+
     public bool Allowed;
+
 }
 [Serializable]
 public struct Fact
