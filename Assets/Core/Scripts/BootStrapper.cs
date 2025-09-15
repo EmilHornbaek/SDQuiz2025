428542eb83edf93940f001353813a22a4cdc4d92
Philip Nielsen <philipnordnielsen@gmail.com>
2025-09-15 10:29:24 +0200

    Added bootstrapper scene and removed old filler files

--- Assets/Core/Scripts/BootStrapper.cs
+++ Assets/Core/Scripts/BootStrapper.cs
@@ -1,18 +1,19 @@
 using System.Collections;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class BootStrapper : MonoBehaviour
 {
     [Header("Scenes to load (Must be in Build Settings)")]
-    [SerializeField] private string mainSceneName;
-    [SerializeField] private string uiSceneName;
+    [SerializeField] private SceneAsset mainScene;
+    [SerializeField] private SceneAsset uiScene;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     
     private IEnumerator Start()
     {
-        yield return SceneManager.LoadSceneAsync(mainSceneName, LoadSceneMode.Additive);
-        yield return SceneManager.LoadSceneAsync(uiSceneName, LoadSceneMode.Additive);
+        yield return SceneManager.LoadSceneAsync(mainScene.name, LoadSceneMode.Additive);
+        yield return SceneManager.LoadSceneAsync(uiScene.name, LoadSceneMode.Additive);
         yield return SceneManager.UnloadSceneAsync(gameObject.scene);
     }
 }
