03e3ed40d26dd6d8e5cbb4872a85dc8226b788c8
Philip Nielsen <philipnordnielsen@gmail.com>
2025-09-16 22:40:16 +0200

    Fully working LinearMovement &amp; Transform extension to make snapshots of a transform (Usefull for Interpolation since Transform is a class) &amp; Added Inspector extension to name fields [FieldName(&quot;Name&quot;)] &lt;- this shows in the inspector as field name

--- Assets/Core/Scripts/LinearMovement.cs
+++ Assets/Core/Scripts/LinearMovement.cs
@@ -1,47 +1,90 @@
 using System;
 using UnityEngine;
+using Nullzone.Unity.Attributes;
+using Unity.VisualScripting;
 
 [Serializable]
 public enum MovementType
 {
     Linear,
-    BiLinear,
-    Cubed
+    Cubic
 }
+[Serializable]
+public enum WorldSpaceType
+{
+    [InspectorName("2D")]
+    SecondDimension,
+    [InspectorName("3D")]
+    ThirdDimension
+}
+
 public class LinearMovement : MonoBehaviour
 {
-    [SerializeField] private float duration;
+    private const int WEIRD_MAGIC_DURATION_MULTIPLIER = 5;
+    [SerializeField, FieldName("Duration (seconds)")] private float duration;
     [SerializeField] private MovementType lerpMethod = MovementType.Linear;
+    [SerializeField] private WorldSpaceType worldSpaceType;
     
     private bool active = false;
     private float timer = 0f;
 
-    private Transform currentDestination;
-    private Transform activeDestination;
+    private TransformSnapshot currentDestination;
+    private TransformSnapshot activeDestination;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        currentDestination = transform;        
+        currentDestination = transform.Snapshot();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (active && activeDestination is not null)
+        if (active && !activeDestination.Equals(default))
         {
-            transform.position = Vector3.Lerp(currentDestination.position, activeDestination.position, Math.Min(timer / duration, 1f));
+            timer += Time.deltaTime;
+
+            float t = HandleInterpolation(timer / duration);
+            HandleTransform(t);
+
 
             if (timer >= duration)
             {
                 currentDestination = activeDestination;
-                activeDestination = null;
+                activeDestination = default;
+                timer = 0;
                 active = false;
             }
 
 
-            timer += Time.deltaTime;
         }
+    }
+
+    private void HandleTransform(float t)
+    {
+        switch (worldSpaceType)
+        {
+            case WorldSpaceType.SecondDimension:
+                Vector3 activeVector = activeDestination.position;
+                transform.position = Vector3.Lerp(currentDestination.position, new Vector3(activeVector.x, activeVector.y, currentDestination.position.z), t);
+                break;
+            case WorldSpaceType.ThirdDimension:
+                transform.position = Vector3.Lerp(currentDestination.position, activeDestination.position, t);
+                break;
+            default:
+                break;
+        }
+    }
+
+    private float HandleInterpolation(float v)
+    {
+        switch (lerpMethod)
+        {
+            case MovementType.Cubic:
+                v = Cubic(v);
+                break;
+        }
+        return Mathf.Clamp01(v);
     }
 
     public void Activate()
@@ -51,7 +94,12 @@ public class LinearMovement : MonoBehaviour
 
     public void GoTo(Transform destination)
     {
-        activeDestination = destination;
+        activeDestination = destination.Snapshot();
         active = true;
+    }
+
+    private float Cubic(float t)
+    {
+        return t < .5f ? 4f * t * t * t : 1f - Mathf.Pow(-2f * t + 2f, 3f) / 2f;
     }
 }
