34f70fd13de37c59c2f8df061e4083ed0c586a50
madmax1993 <madmax199308@gmail.com>
2025-09-17 13:44:23 +0200

    Fixed UI for clouds and background to not be handled on a canvas in crazy scale.

--- Assets/Core/Scripts/BackgroundUI/BGMoveLeftAndReset.cs
+++ Assets/Core/Scripts/BackgroundUI/BGMoveLeftAndReset.cs
@@ -2,10 +2,10 @@ using UnityEngine;
 
 public class BGMoveLeftAndReset : MonoBehaviour
 {
-    [SerializeField] float minSpeed = 40f;
-    [SerializeField] float maxSpeed = 80f;
-    [SerializeField] float resetDistance = 800f;
-    [SerializeField] float verticalOffsetOnReset = 50f;
+    [SerializeField] float minSpeed = 0.2f;
+    [SerializeField] float maxSpeed = 0.5f;
+    [SerializeField] float resetDistance = 8f;
+    [SerializeField] float verticalOffsetOnReset = 2f;
     private float speed;
     Vector3 startPos;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -23,6 +23,7 @@ public class BGMoveLeftAndReset : MonoBehaviour
         {
             transform.position = startPos;
             transform.position += new Vector3(0, Random.Range(-verticalOffsetOnReset, verticalOffsetOnReset), 0);
+            speed = Random.Range(minSpeed, maxSpeed);
         }
     }
 }
